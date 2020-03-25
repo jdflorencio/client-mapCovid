b624cc551220a38c62ad3b4df9899e1b7296a3b1
João Diego <jdflorencio@gmail.com>
2020-03-25 16:38:37 -0300

    Onde iniciar - se todos os modulos

--- src/Modules/Modulos.js
+++ src/Modules/Modulos.js
@@ -1,4 +1,11 @@
-import { ExempleController } from './exemple'
-.component('')
+import angular from 'angular'
+import {ExempleComponent} from './exemple/Exemple'
+import {OtherComponent} from './other/Other'
+
+const Modulos = 'modulos'
+angular.module(Modulos, [])
+
+.component('exemple', ExempleComponent)
+.component('other', OtherComponent)
 
 export default Modulos
