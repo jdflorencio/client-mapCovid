77a4d90a99a897309c60c77eb16ce8531423aba3
João Diego <jdflorencio@gmail.com>
2020-03-25 16:35:02 -0300

    modulo 2

---
+++ src/Modules/other/Other.js
@@ -0,0 +1,13 @@
+// import template from './Other.html'
+// import '/Exemple.scss'
+
+class OtherController {
+    constructor() {
+        console.warn("MODULE DE OTHER 2....")
+    }
+}
+
+export const OtherComponent = {
+    controller: OtherController,    
+    
+}
