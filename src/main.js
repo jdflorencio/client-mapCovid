76436a741af7006481ceced6dd44a8a2dd8464e7
João Diego <jdflorencio@gmail.com>
2020-03-25 16:37:01 -0300

    onde tudo se inicia

--- src/main.js
+++ src/main.js
@@ -2,20 +2,13 @@ import angular from 'angular'
 import '@uirouter/angularjs'
 import 'material-design-lite'
 import 'material-design-lite/material'
-import { MainController } from './app.controller'
-import router from './configRoutes'
-// import { MDCRipple } from '@material/ripple/index'
+import Modulos from './Modules/Modulos'
+import configRoute from './configRoutes'
 
-// const ripple = new MDCRipple(document.querySelector('.foo-button'))
-
-var materialApp = angular
-.module('materialApp',  [
-    router
+export const app = 'app'
+angular.module('app',  [
+    'ui.router',
+    Modulos
 ])
 .constant('API', 'http://127.0.0.1:3333/')
-// .config(routesConfig)
-.controller('appCtrl', function(){
-    console.warn('DENTRO DO CONTROLLER PRINCIPAL')
-})
-
-
+.config(configRoute)
